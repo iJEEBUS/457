06edd8e98b9797bbf8b6a8fe22dd978e8464913d
Ronald Rounsifer <ronrounsifer@me.com>
2018-09-27 14:28:00 -0400

    Client and server now connect. Server can receive and interpret the commands that the client sends.

--- Project 1/ftp_client.java
+++ Project 1/ftp_client.java
@@ -13,19 +13,46 @@
  * @author Ron Rounsifer
  * @version 9.26.2018
  */
+import java.io.*;
 import java.net.*;
+import java.util.Date;
+import java.util.Scanner;
 
 public class ftp_client {
+
 	/**
 	* Creates an instance of a client and connects to the server
 	* that is hardcoded into the program.
 	*
 	* @param args
 	*/
-	public static void main(String[] args) {}
+	public static void main(String[] args) throws IOException {
+
+		// connect to server and setup data connection
+		Socket server = new Socket("127.0.0.1", 1234);
+		System.out.println("Connected to " + server.getInetAddress());
+		DataOutputStream out = new DataOutputStream(new BufferedOutputStream(server.getOutputStream()));
+
+		// Prompt user for input
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a command: ");
+		String input = scan.nextLine();
+		byte[] buffer = input.getBytes();
+
+
+		// Send the string to buffer then server
+		out.write(buffer, 0, buffer.length);
+		out.flush();
+
+
+
+	}
  
   /**
    * Terminates the connection with the server
    */
-   private void quit() {}
+   private void quit(DataOutputStream dos) throws IOException {
+   	dos.close();
+
+   }
 }
--- Project 1/ftp_server.java
+++ Project 1/ftp_server.java
@@ -5,7 +5,11 @@
  * @author Ron Rounsifer
  * @version 9.26.2018
  */
-import java.net.*;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.*; // for Socket, ServerSocket, and InetAddress
+import java.util.Scanner;
 
 public class ftp_server {
 
@@ -15,8 +19,48 @@ public class ftp_server {
    *
 	 * @param args
 	 */
-	public static void main(String[] args) {}
-  
+	public static void main(String[] args) throws IOException {
+
+
+        // Create the server socket with the specified port
+        int servPort = 1234;
+        ServerSocket servSocket = new ServerSocket(servPort);
+
+
+        // Keep server up and listening for new connections
+        while (true) {
+
+
+            // Accept connection
+            System.out.println("Waiting for client to connect...");
+            Socket client = servSocket.accept();
+            System.out.println("Client from " + client.getInetAddress() + " connected");
+
+            // Print command
+            DataInputStream in = new DataInputStream(new BufferedInputStream(client.getInputStream()));
+            String command = convertStreamToString(in);
+            System.out.println("Command: " + command);
+        }
+    }
+
+
+    /**
+     * A helper method that converts the bytes from the data input stream into
+     * a string that we can use to control the flow of the program based on the
+     * users input.
+     *
+     * @param is - DataInputStream : the byte data received from client
+     * @return String - : the decoded version of the byte data
+     */
+    private static String convertStreamToString(DataInputStream is) {
+	    Scanner scan = new Scanner(is).useDelimiter("\\A");
+	    return scan.hasNext() ? scan.next() : "";
+    }
+
+    
+
+
+
   /**
    * Lists all of the files that are in the current directory of the 
    * server
