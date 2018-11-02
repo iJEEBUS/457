78ae0c515904fb5cf8be0ef3e5ad1b3cfb789091
Ronald Rounsifer <ronrounsifer@me.com>
2018-11-02 09:02:52 -0400

    Adding beginnings of server and client classes

--- ProjectTwo/GUI.py
+++ ProjectTwo/GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import Client
 """GUI
 
 User interface for a peer-to-peer application that will allow 
@@ -19,6 +20,8 @@ def connect():
 	port = port_entry.get()
 	user = username_entry.get()
 	local_hostname = hostname_entry.get()
+	print(server_hostname, port)
+
 
 def setSpeed(selection):
 	'''Set the speed limit
@@ -53,29 +56,28 @@ window = Tk()
 window.title("Napster Host")
 window.configure(background="white")
 
-##### Connection
-# Connection Label
+##### Connection panel
+# Labels
 Label (window, text="Connection", bg="white", fg="black").grid(row=0, column=0, sticky=W)
-# Server Hostname
 Label (window, text="Server Hostname:", bg="white", fg="black").grid(row=1, column=0, sticky=W)
-hostname_entry = Entry(window, width=20, bg="white")
-hostname_entry.grid(row=1, column=1, sticky=W)
-# Port Number
 Label (window, text="Port:", bg="white", fg="black").grid(row=1, column=2, sticky=W)
+Label (window, text="Username:", bg="white", fg="black").grid(row=2, column=0, sticky=W)
+Label (window, text="Hostname:", bg="white", fg="black").grid(row=2, column=2, sticky=W)
+Label (window, text="Speed:", bg="white", fg="black").grid(row=2, column=4, sticky=W)
+
+# Button
+Button(window, text="Connect", width=15, command=connect).grid(row=1, column=5, sticky=E)
+
+# Text Inputs
 port_entry = Entry(window, width=7, bg="white")
 port_entry.grid(row=1, column=3, sticky=W)
-# Connect
-Button(window, text="Connect", width=15, command=connect).grid(row=1, column=5, sticky=E)
-# Username
-Label (window, text="Username:", bg="white", fg="black").grid(row=2, column=0, sticky=W)
 username_entry = Entry(window, width=20, bg="white")
 username_entry.grid(row=2, column=1, sticky=W)
-# Hostname
-Label (window, text="Hostname:", bg="white", fg="black").grid(row=2, column=2, sticky=W)
 hostname_entry = Entry(window, width=20, bg="white")
-hostname_entry.grid(row=2, column=3, sticky=W)
-# Speed
-Label (window, text="Speed:", bg="white", fg="black").grid(row=2, column=4, sticky=W)
+hostname_entry.grid(row=1, column=1, sticky=W)
+
+
+# Dropdown menu
 speeds = ["Ethernet", "Option 2", "Option 3"]
 default = StringVar(window)
 default.set(speeds[0])
@@ -85,16 +87,18 @@ speeds_menu.grid(row=2, column=5)
 
 ## Adding a blank space between connection and search areas
 Label(window, text="Blank Space", bg="white", fg="white").grid(row=3, column=0, sticky=W)
-##### Search
-# Search label
+
+
+##### Search Panel
+# Labels
 Label (window, text="Search", bg="white", fg="black").grid(row=4, column=0, sticky=W)
-# Keyword
 Label (window, text="Keyword:", bg="white", fg="black").grid(row=5, column=0, sticky=W)
+# Button
+Button(window, text="Search", width=15, command=search).grid(row=5, column=2, sticky=W)
+# Text inputs
 keyword_entry = Entry(window, width=20, bg="white")
 keyword_entry.grid(row=5, column=1, sticky=W)
-# Search button
-Button(window, text="Search", width=15, command=search).grid(row=5, column=2, sticky=W)
-
+#Table
 #TODO make this a response table
 # Server response table to be implemented
 Label (window, text="Table", width=20, bg="white").grid(row=6, column=0, sticky=W)
@@ -103,13 +107,21 @@ Label (window, text="Here", width=20, bg="white").grid(row=8, column=0, sticky=W
 Label (window, text="Please", width=20, bg="white").grid(row=9, column=0, sticky=W)
 Label (window, text="Thank you", width=20, bg="white").grid(row=10, column=0, sticky=W)
 
-##### FTP
+
 ## Adding a blank space between connection and search areas
 Label(window, text="Blank Space", bg="white", fg="white").grid(row=11, column=0, sticky=W)
-Label (window, text="Enter Command: ", bg="white", fg="black").grid(row=12, column=0, sticky=W)
-command_entry = Entry(window, width=20, bg="white", fg="black").grid(row=12, column=1, sticky=W)
-Button(window, text="Go", width=15, command=go).grid(row=12, column=2, sticky=W)
 
+
+##### FTP panel
+# Labels
+Label (window, text="Enter Command: ", bg="white", fg="black").grid(row=12, column=0, sticky=W)
+# Button
+Button(window, text="Go", width=15, command=go).grid(row=12, column=2, sticky=W)
+# Text inputs
+command_entry = Entry(window, width=20, bg="white", fg="black")
+command_entry.grid(row=12, column=1, sticky=W)
+# Table
+#TODO make this a response table
 Label (window, text="Table", width=20, bg="white").grid(row=13, column=0, sticky=W)
 Label (window, text="Goes", width=20, bg="white").grid(row=14, column=0, sticky=W)
 Label (window, text="Here", width=20, bg="white").grid(row=15, column=0, sticky=W)
