3d6454b49047ad3d5d6c6dddc646204659f85fa0
iJEEBUS <ronrounsifer@me.com>
2018-11-16 12:53:18 -0500

    Working on server acknowledgments to the peers.

--- ProjectTwo/CentralServer.py
+++ ProjectTwo/CentralServer.py
@@ -6,21 +6,32 @@ manage with the BabyGit version control software.
 @author Ron Rounsifer, Bryce Hutton
 @version 11.12.2018 (10.26.2018)
 '''
+from ftplib import FTP
 import os
-import xml.etree.ElementTree
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import ThreadedFTPServer
 from pyftpdlib.authorizers import DummyAuthorizer
 
 class ServerHandler(FTPHandler):
     registered_users = {}
+    ftp = None
+
+    def sendAckToPeer(self, hostname, port,  file):
+        self.ftp = FTP()
+        self.ftp.connect()
+        self.ftp.login()
+        self.ftp.cwd('.')
+
+        self.ftp.storbinary('STOR' + file, open(file, 'rb'))
+
+
 
     def on_file_received(self, file):
         '''Populates databases with given information
-        
+
         Given the xml file passed through, this method parses the information
          it contains and adds it to the corresponding data table in our database.
-        
+
         Arguments:
             file File -- register / file_list file uploaded to server
         '''
@@ -44,7 +55,15 @@ class ServerHandler(FTPHandler):
             if username not in self.registered_users.keys():
                 self.registered_users[username] = {"hostname" : local_host_address, "speed" : connection_speed}
             else:
-                print ("This username is already registered.")
+                # Create the error file and send it to the peer
+                error_message = "This username is already registered."
+                with open('failure.txt', 'w') as failure_file:
+                    failure_file.write(error_message)
+                    self.sendAckToPeer(failure_file)
+
+                # Send the error file to the peer
+
+
         elif filename == file_list:
             if username not in self.registered_users.keys():
                 print ("Please register your account before sharing files!")
@@ -53,7 +72,7 @@ class ServerHandler(FTPHandler):
                 #  Add this to the file_list database table
                 # SHOULD INCLUDE:
                 # username, filenames, and file descriptions
-            pass
+                pass
         for x in self.registered_users:
             print(x)
         
--- ProjectTwo/Peer.py
+++ ProjectTwo/Peer.py
@@ -1,6 +1,17 @@
 from ftplib import FTP
 import xml.etree.cElementTree as ET
 
+import os
+
+
+import time
+import xml.etree.ElementTree
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import ThreadedFTPServer
+from pyftpdlib.authorizers import DummyAuthorizer
+
+
+
 class Peer(object):
     # FTP instance
 	ftp = None
@@ -75,6 +86,7 @@ class Peer(object):
 		Returns:
 			bool -- The client-server connection status
 		'''
+		# Create connection to server
 		string_server_name = str(server_name)
 		int_port = int(port)
 		print("Attempting connection to " + server_name + " on port " + port)
@@ -87,11 +99,58 @@ class Peer(object):
 		self.createRegistrationXML(user, local_host, speed)
 		print("Registering: " + user + "...")
 		registration_file = "registration.xml"
+
+		# Send the registration file to the server
 		self.ftp.storbinary('STOR ' + registration_file, open(registration_file, 'rb'))
+
+		# Creates a temp server and waits for the server to send
+		#  an acknowledgement that the user has registered successfully.
+		timeout= time.time() + 60 # Set the time out to 60 seconds
+		while time.time() < timeout:
+
+			authorizer = DummyAuthorizer()
+			authorizer.add_anonymous('.', perm="elradfmw")
+
+			handler = PeerHandler
+			handler.authorizer = authorizer
+			temp_server = ThreadedFTPServer("", 1010)
+			temp_server.serve_forever()
+
+
 
 		self.__CONNECTION_ALIVE = True
 
 		return self.__CONNECTION_ALIVE
+
+
+class PeerHandler(FTPHandler):
+
+	def on_file_received(self, file):
+		'''
+		When the server sends the acknowledgement file back to the peer
+		it will either be named "success.txt" or "failure.txt".
+
+		The success file will be empty.
+		The failure file will have the error written to the file for further
+		investigation.
+
+		:param file :  acknowledgement file sent from the server
+		'''
+
+		# Extract just the files name
+		filename = os.path.basename(file)
+		registration_success = "success.txt"
+		registration_failure = "failure.txt"
+
+		# Check the servers response.
+		# Delete success file if needed, leave failure file since
+		# it contains any errors produced.
+		if file == registration_success:
+			print ("Your account has been registered by the server.")
+			os.remove(file)
+		elif file == registration_failure:
+			print ("There was an error while registering your account.")
+			print ("Check the file 'failure.txt' for any errors produced.")
 
 
 
