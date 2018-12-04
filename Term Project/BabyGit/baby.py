3e2afecbfa103c0abb073cfc98f9dd9f40966369
huttonb <huttonb@mail.gvsu.edu>
2018-12-04 01:08:39 -0500

    Added help command and user command.
    Added the ability to change username and the ability to use Help to look at valid commands.

--- Term Project/BabyGit/baby.py
+++ Term Project/BabyGit/baby.py
@@ -26,10 +26,10 @@ class Baby(Client):
         self.host_address = "0.0.0.0"
         self.cwd = os.getcwd()
         self.directory = self.cwd + "/"
-        self.bbydir = self.directory + ".babygit"
+        self.bby_dir = self.directory + ".babygit"
         self.head = (self.directory + ".babygit/HEAD.ibby")
-        self.user = "Bryce"
-        self.repoName = "babygit"
+        self.user = None
+        self.repo_name = "babygit"
         #todo add user to parse
         # file_list_index is the index of where the list of files in version control in the header ends.
         self.file_list_end_index = None
@@ -65,12 +65,21 @@ class Baby(Client):
             pass
         elif command == "branch":  # Are we adding branch to our program?
             pass
+        elif command == "user":
+            self.userChange()
+        elif command == "help":
+            print("Command list:\ninit: initialize a repo.\nstage: stage a file."
+                  "\ncommit: commit changes.\npush: push changes to remote. \nuser: change user")
+            #todo add the rest of the commands as we complete them.
+        else:
+            print("Command not recognized. Use command \"help\" for more information.")
 
     #### Method Definitions ####
     '''Stage stages the file by adding it to the head. Doesn't add directories.'''
-    #todo add ability to stage directories.
     def stage(self, filename):
         fhead = open(self.head, "w")
+        #if the file isn't a directory, rewrite the head with the new file added.
+        # todo add ability to stage directories.
         if (os.path.isfile(filename)):
             self.file_contents.insert(self.file_list_end_index, filename)
             str1 = '\n'.join(self.file_contents)
@@ -80,17 +89,35 @@ class Baby(Client):
         fhead.close()
         pass
 
+    '''Changes the user in the header'''
+    def userChange(self):
+        fhead = open(self.head, "w")
+        new_name = "anon"
+        if len(self.args) == 1:
+            new_name = input("Please enter desired name.")
+        elif len(self.args) == 2:
+            new_name = self.args[1]
+        else:
+            print("Too many arguments entered. Try again.")
+            fhead.close()
+            return
+        self.file_contents[5] = new_name
+        str1 = '\n'.join(self.file_contents)
+        fhead.write(str1)
+        fhead.close()
+        print("Username changed to " + new_name)
+
     '''Commit changes to the file.'''
     def commit(self):
         # Initialize the repository
-        # todo: Change the version.
+        # todo: Change the version, pref to hash of time & user to make unique.
         version = str(self.last_version + 1)
         destfile = self.directory + ".babygit" + "\\vers" + str(version)
         os.makedirs(destfile)
 
         message = ""
 
-        #If there was no comment added to the commit.
+        # create a comment for the commit.
         if len(self.args) == 1:
             message = input("Please enter description for commit: ")
         elif len(self.args) == 2:
@@ -99,18 +126,17 @@ class Baby(Client):
             print("Incorrect commit comment format. Either put comment in between "
                   "\"quotes\" or leave comment empty")
 
+        # Update the last version in the head file.
         fhead = open(self.head, "w")
         self.file_contents[1] = "vers"+version
         self.file_contents.append("vers"+version)
         str1 = '\n'.join(self.file_contents)
         fhead.write(str1)
-        '''
-        fhead = open(self.head, "a")
-        fhead.write("\nvers" + version)
-        '''
         fhead.close()
+
+        # Create a comment file w/ relevant information.
         comment = open(destfile + "/" ".comment.ibby", 'w')
-        comment.write("REPO:" + self.repoName+ "\n")
+        comment.write("REPO:" + self.repo_name + "\n")
         comment.write("TIME:" + str(datetime.datetime.now()) + "\n")
         comment.write("USER:"+self.user+ "\n")
         comment.write("VERS:"+version+ "\n")
@@ -118,22 +144,22 @@ class Baby(Client):
         comment.write(message+ "\n")
         comment.write("-ENDCOMMENT-"+ "\n")
 
-
-        '''For each file in the directory that is listed and staged in the git file, compress it'''
+        # For each file in the directory that is listed and staged in the git file, compress it
         print("Committing files:")
         for filename in os.listdir(self.cwd):
             if (filename in self.baby_files):
                 print("+ " + filename)
                 # If the file is not a directory
+                # todo add directory commit
                 if os.path.isfile(os.path.join(self.directory, filename)):
                     self.__compileFile(filename, destfile + "/" +
                                        filename + '.' + version + '.bby')
 
-
     '''Pushes the file to the remote server.'''
     def push(self):
-        os.chdir(self.bbydir)
+        os.chdir(self.bby_dir)
         super(Baby, self).__init__(self.host_address, self.user)
+        # Try to make a directory server-side, if it fails then no need to push.
         try:
             self.ftp.mkd(self.user + "vers" + str(self.last_version))
             self.ftp.cwd(self.user + "vers" + str(self.last_version))
@@ -145,19 +171,22 @@ class Baby(Client):
 
     '''Recursive loop that pushes files and directories within babygit.'''
     def pushLoop(self, file, curdir):
-        for filename in os.listdir(file):
-            if os.path.isfile(os.path.join(curdir, filename)):
-                self.uploadFile(filename)
-            elif os.path.isdir(os.path.join(curdir, filename)):
-                self.ftp.mkd(filename)
-                self.ftp.cwd(filename)
-                curdir1 = curdir + "/" + filename
+        for file_name in os.listdir(file):
+            # If the given file is a file, upload to server
+            if os.path.isfile(os.path.join(curdir, file_name)):
+                self.uploadFile(file_name)
+            # If the file is a directory, make a new directory server-side, switch to it client
+            # and server side,and recurse. After recurse, switch back to previous directory.'''
+            elif os.path.isdir(os.path.join(curdir, file_name)):
+                self.ftp.mkd(file_name)
+                self.ftp.cwd(file_name)
+                curdir1 = curdir + "/" + file_name
                 os.chdir(curdir1)
-                self.pushLoop(file + filename, curdir1)
+                self.pushLoop(file + file_name, curdir1)
                 self.ftp.cwd("..")
                 os.chdir("..")
 
-    # Function checks to see if the file is version controlled by baby
+    ''' Parses through the head file and grabs relevant information'''
     def __headParse(self):
         header = open(self.head, 'r')
         temp = header.read()
@@ -181,6 +210,10 @@ class Baby(Client):
                 self.host_address = contents[index+1]
             elif line == "-LOCALHEAD":
                 self.local_head = contents[index+1]
+            elif line == "-USER":
+                self.user = contents[index + 1]
+            elif line == "-REPONAME":
+                self.repo_name = contents[index + 1]
             else:
                 if listing_files:
                     listed_files.append(line)
@@ -196,13 +229,11 @@ class Baby(Client):
         header.close()
         return
 
-    # Given a file and a destination this function compiles and creates a new file
-    def __compileFile(self, filename, new_filename):
-        fin = open(filename, 'rb')
-        # Todo: Add identifier for the file
-        # todo: Maybe a specific identifier for each repo cloned
-        # todo: Then an identifier for each version from that repo
-        fout = gzip.open(new_filename, 'wb')
+    '''Given a file and a destination this function compiles and creates a new file'''
+    def __compileFile(self, file_name, new_file_name):
+        fin = open(file_name, 'rb')
+        # todo: add repo identifier for each repo cloned
+        fout = gzip.open(new_file_name, 'wb')
         fout.writelines(fin)
         fout.close()
         fin.close()
@@ -284,18 +315,5 @@ class Baby(Client):
 
 #### Script ####
 args1 = []
-#args1.append("commit")
-#args1.append("testfile.txt")
 args1 = sys.argv[1:]
 b = Baby(args1)
-'''
-# Initialize a BabyGit repo
-if command == "init":
-	# Assign name to repo if passed
-	if len(args) == 2:
-		repo_name = args[1]
-	else:
-		repo_name = None
-	# Initialize the repository
-	repoInit(repo_name)
-'''
